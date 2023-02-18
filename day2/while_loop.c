12d9eca5ad24455751b58f6e93b69005e8b3f60a
Pitambar Yadav <pitambaryadav.info.np@gmail.com>
2023-02-18 08:39:01 +0545

    Added while loop

---
+++ day2/while_loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n;
+    int i=0;
+
+printf("Enter the number upto which you want to perform the task");
+scanf("%d",&n);
+while (i<=n)
+{
+printf("The number is %d\n",i++);
+}
+
+
+    return 0;
+}
